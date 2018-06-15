a3e034ce07d72cd5dc7d23987219411bd8d2243e
Matt <mjtinklerfairbairn@gmail.com>
2018-06-15 18:15:38 +0000

    JSON file is now used to store bot configuration as well as the last known IP address instead of janky .cfg and text reading All the different send messages functions are now only one function, although there is now only need for one message anyway CheckConnection is ran before any web connections are tried, therefore there is no need to sleep at the beginning on the script

--- IPUpdate.py
+++ IPUpdate.py
@@ -3,69 +3,32 @@ import os
 import time
 import socket
 import platform
-# When launched on boot, time is needed for a network  connection to be established
-time.sleep(15)
+import json
 
-###Config###
-SettingsFile = "IPUpdateSettings.cfg"  # Location of settings file to be used
+##Config##
+SettingsFile= "IPUpdateSettings.json"
+with open(SettingsFile) as JSONFile:
+    Settings = json.load(JSONFile)
 
+def GetSetting(SettingType, SettingName, Settings=Settings):
+        return (Settings[SettingType])[SettingName]
 
-def GetSetting(Option, File=SettingsFile):
-    with open(File) as Settings:
-        for line in Settings:
-            if Option in line:
-                SettingResult = line.split("=", 1)[1]
-                SettingResult = SettingResult.rstrip()
-                return SettingResult
+def ChangeSetting(SettingType, SettingName, NewValue, Settings=Settings):
+    (Settings[SettingType])[SettingName] = NewValue
+    with open(SettingsFile, 'w+') as JSONFile:
+        json.dump(Settings,JSONFile)
 
+ChatID = GetSetting("BOTConfig","ChatID")  # Chat ID for telegram user/group
+URL = "http://api.telegram.org/bot{0}/sendMessage".format(GetSetting("BOTConfig","Token")) #URL with bot token for sending message
+Prefix = "[{0}] ~".format(socket.gethostname())
 
-CHATID = GetSetting("CHATID")  # Chat ID for telegram user/group
-TOKEN = GetSetting("TOKEN")  # Bot token for telegram bot
-URL = "http://api.telegram.org/bot{0}/sendMessage".format(TOKEN)
-prefix = "[{0}] ~".format(socket.gethostname())
-SavedLastIP = GetSetting("SavedLastIP")
+##Telegram Function##
+def SendMessage(Message):
+    print("Sending message: {0}".format(Message))
+    r = requests.post("{0}?chat_id={1}&text={2}".format(URL, ChatID, Message))
 
-
-def sendmsg(ip):
-    print("Sending IP change message")
-    MSG = "{0} Current IP address has changed! New IP is now: {1}".format(
-        prefix, ip)
-    r = requests.post("{0}?chat_id={1}&text={2}".format(URL, CHATID, MSG))
-
-
-def sendmsg404(ip):
-    print("Sending 404 telegram message")
-    MSG = "{0} Lastip address was not found! Current IP is: {1}".format(
-        prefix, ip)
-    r = requests.post("{0}?chat_id={1}&text={2}".format(URL, CHATID, MSG))
-
-###IP Functions###
-
-
-def getip():
-    # Get IP Address using puiblic API
-    website = (requests.get('http://api.ipify.org')).text
-    ip = website.strip()
-    print("Current Public IP is: {0}".format(ip))
-    return ip
-
-
-def getLip():
-    # Read Last IP from file
-    txt = open(SavedLastIP, "r")
-    lastip = txt.read()
-    txt.close()
-    print("Last IP is: {0}".format(lastip))
-    return lastip
-
-
-def setLip(ip):
-    #Write IP To lastip file#
-    txt = open(SavedLastIP, "w")
-    txt.write(ip)
-    txt.close()
-
-
+##Main Functions##
+#ConnectionCheck to ensure you're connected before attempting to grab an IP
 def CheckConnection(connected=False):
     # Check for web connection
     OS = platform.platform()
@@ -81,32 +44,26 @@ def CheckConnection(connected=False):
         else:
             print("Network Bad will loop!")
 
+# Get IP Address using puiblic API
+def GetIP():
+    IP = ((requests.get('http://api.ipify.org')).text).strip()
+    print("Current Public IP is: {0}".format(IP))
+    return IP
 
-def checkip(LastIP):
+def CheckIP(LastIP):
     # Main loop for checking public IP address change
     while True:
         print("\nLooping\n")
-        CheckConnection()  # Make sure still connected before trying to get an IP!
-        CurrentIP = getip()
+        CheckConnection()  # Check device is still connected to the net
+        CurrentIP = GetIP()
         if CurrentIP == LastIP:
             print("IP matched! Sleeping for a few minutes!")
             time.sleep(300)
         else:
             print("IP changed!!!")
-            sendmsg(CurrentIP)
-            setLip(CurrentIP)
+            SendMessage("{0} Current IP address has changed! New IP is now: {1}".format(Prefix, CurrentIP))
+            ChangeSetting("Data","LastIP",CurrentIP)
             LastIP = CurrentIP
 
-
-###MAIN###
-# Check if there is a last IP Address
-if os.path.exists(SavedLastIP):
-    print("Last IP address found!")
-else:
-    print("Last IP address not found")
-    CurrentIP = getip()
-    sendmsg404(CurrentIP)
-    setLip(CurrentIP)
-
 # Run Main loop
-checkip(getLip())
+CheckIP(GetSetting("Data","LastIP"))
